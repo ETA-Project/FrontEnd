63b5353a8f72de686129b14f50a94cd4903bfd06
heeyeon <dlgmldus3289@naver.com>
2024-01-05 02:35:41 +0900

    버튼사진추가

--- src/components/elevator.js
+++ src/components/elevator.js
@@ -1,9 +1,12 @@
 import imgLogo from "../elevator.png";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import up from "../up.png";
+import down from "../down.png";
 
 export default function Elevator(props) {
-  const [data, setData] = useState("0");
+  const [data, setData] = useState("1");
+  const [prevData, setPrevData] = useState("1");
   const baseUrl =
     "http://ec2-13-124-153-108.ap-northeast-2.compute.amazonaws.com:8080";
 
@@ -16,7 +19,9 @@ export default function Elevator(props) {
       console.log(e);
     }
   }
+
   useEffect(() => {
+    setPrevData((prev) => prevData);
     callApi();
   }, [data]);
 
@@ -39,7 +44,11 @@ export default function Elevator(props) {
         style={{ display: "block", margin: "0 auto" }}
         alt="Elevator Logo"
       />
-      <h1>{data}</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {prevData > data && <img className="arrow" src={down} alt="down" />}
+        {prevData < data && <img className="arrow" src={up} alt="up" />}
+        <h1 style={{ display: "inline-block" }}>{data}</h1>
+      </div>
     </div>
   );
 }
