b27d12dd11e8a1a361f9f1d55c6f9d377ce6b9a8
heedong12 <dlgmldus3289@naver.com>
2024-03-30 17:58:32 +0900

    [design] 기본화면 Main으로 변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Elevator1 from "./pages/elevator1";
 import Elevator2 from "./pages/elevator2";
 import Elevator3 from "./pages/elevator3";
 import Elevator4 from "./pages/elevator4";
-import Home from "./pages/home";
+import Main from "./pages/Main";
 import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/elevator1" element={<Elevator1 />} />
         <Route path="/elevator2" element={<Elevator2 />} />
         <Route path="/elevator3" element={<Elevator3 />} />
